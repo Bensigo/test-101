537dbfa129d68f2480e39c51911564dff942dab7
larry sigo <sigocode@gmail.com>
2016-07-03 15:06:21 +0100

    updated the settings/production.py ,.gitignore,wsgi.py

---
+++ settings/production.py
@@ -0,0 +1,13 @@
+import dj_database_url
+
+
+DATABASES[‘default’] = dj_database_url.config()
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
+
+STATIC_URL = '/static/'
+ STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'),
+ )
+ STATIC_ROOT = 'staticfiles'
